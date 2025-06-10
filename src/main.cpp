73712d744155ed8aa52777eedfb96639128d1069
21ntphilos <ucheokoye99@gmail.com>
2025-06-10 15:59:39 +0100

    REPL echo2

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,13 @@ int main() {
   // std::cout << input << ": command not found " << std::endl;
 
   while(true){
-    std::cout<<"$ ";
+    // std::cout<<"$ ";
     std::getline(std::cin, input);
 
     if(input == "exit" || input == "exit 0"){
       break;
     }
-    if(input.find("echo") == 0){
+    if(input.find("echo ") == 0){
       std::cout<< input.substr(5) << std::endl;
     }
 
