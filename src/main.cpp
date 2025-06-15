5567ba0503d8da3eb3fb35a6996d2eb049634c23
21ntphilos <ucheokoye99@gmail.com>
2025-06-15 11:20:30 +0100

    REPL Debug echo6

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ int main() {
     const int n = input.find("echo ", 0);
     if(n == 0){
       std::cout<< input << " "<< n << std::endl;
-      std::cout << input.substr(n) << std::endl;
+      std::cout << input.substr(input.length() - 5) << std::endl;
       break;
     }
 
