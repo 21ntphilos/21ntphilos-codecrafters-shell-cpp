6abb537c68d4df717b8dfd26c40d20e9b24d1c5b
21ntphilos <ucheokoye99@gmail.com>
2025-06-16 16:21:13 +0100

    REPL Built-in Command PATH

--- src/main.cpp
+++ src/main.cpp
@@ -3,56 +3,52 @@
 #include <vector>
 #include <sstream>
 #include <algorithm>
+#include <cstdlib>
+#include <filesystem>
 
-int main() {
+namespace fs = std::filesystem;
+
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
+    // Uncomment this block to pass the first stage
   // std::cout << "$ ";
 
   std::vector<std::string> built_in_commands = {
-    "type",
-    "echo",
-    "exit",
-    "cd",
-    "pwd",
-    "ls",
-    "cat",
-    "clear",
-    "help"
-  };
+      "type",
+      "echo",
+      "exit",
+      "cd",
+      "pwd",
+      "ls",
+      "cat",
+      "clear",
+      "help"};
 
   std::string input;
   // std::getline(std::cin, input);
 
   // std::cout << input << ": command not found " << std::endl;
 
-  while(true){
-    std::cout<<"$ ";
+  while (true)
+  {
+    std::cout << "$ ";
     std::getline(std::cin, input);
     std::stringstream ss(input);
     std::vector<std::string> args;
     std::string word;
-    while(ss >> word){
+    while (ss >> word)
+    {
       args.push_back(word);
     }
-    if(args.empty()){
+    if (args.empty())
+    {
       continue;
     }
-    const std::string& command = args[0];
-  
-    // for(size_t i = 1; i < args.size(); ++i) {
-    //   if (args[i].find(' ') != std::string::npos) {
-    //     std::cerr << "Error: Arguments should not contain spaces." << std::endl;
-    //     continue;
-    //   }
-    //   std::cout << "Argument " << i << ": " << args[i] << std::endl;
-    // }
-
-
-    // std::cout <<"INPUTE " <<input << std::endl;
+    const std::string &command = args[0];
 
     if (command == "exit" || command == "exit 0")
     {
@@ -69,16 +65,72 @@ int main() {
     }
     if (command == "type")
     {
-      if (std::find(built_in_commands.begin(), built_in_commands.end(), args[1]) != built_in_commands.end()){
+      if (std::find(built_in_commands.begin(), built_in_commands.end(), args[1]) != built_in_commands.end())
+      {
+        const char *path_env = std::getenv("PATH");
+        if (!path_env)
+        {
+          std::cerr << "PATH environment variable not found." << std::endl;
+          return 1;
+        }
 
-        std::cout << args[1] << " is a shell builtin" << std::endl;
-      } else {
-        std::cout << args[1] << ": not found" << std::endl;
+        #ifdef _WIN32
+                char pathDelimiter = ';';
+        #else
+                char pathDelimiter = ':';
+        #endif
+
+        std::string path_string(path_env);
+
+        std::vector<std::string> paths = splitString(path_string, pathDelimiter);
+        fs::path filePath = findFileinpath(args[1], paths);
+        if (!filePath.empty())
+        {
+          std::cout << args[1] << " is " << filePath.string() << std::endl;
+        }
+        else
+        {
+          std::cout << args[1] << ": not found" << std::endl;
+        }
+
+      //   std::cout << args[1] << " is a shell builtin" << std::endl;
+      // }
+      // else
+      // {
+      //   std::cout << args[1] << ": not found" << std::endl;
       }
       continue;
     }
 
-
     std::cout << input << ": command not found " << std::endl;
   }
+}
+
+std::vector<std::string> splitString(const std::string &str, char &separator)
+{
+
+  std::string token;
+  std::vector<std::string> filepaths;
+  std::stringstream pathstreams(str);
+
+  while (std::getline(pathstreams, token, separator))
+  {
+    filepaths.push_back(token);
+  }
+  return filepaths;
+}
+
+fs::path findFileinpath(const std::string &fileName, const std::vector<std::string> &paths)
+{
+
+  for (const std::string &path : paths)
+  {
+    fs::path fullPath = fs::path(path) / fileName;
+    if (fs::exists(fullPath) && fs::is_regular_file(fullPath))
+    {
+      return fullPath;
+    }
+  }
+
+  return fs::path();
 }
