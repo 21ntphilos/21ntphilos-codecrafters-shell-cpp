a2cc7de29afa865949b742b9da4510d451b411bd
21ntphilos <ucheokoye99@gmail.com>
2025-06-25 18:21:20 +0100

    Implement REPL Command echo single quote debug5

--- src/main.cpp
+++ src/main.cpp
@@ -125,7 +125,7 @@ int main()
       // }
       const std::string str = "echo ";
       const int n = input.find("echo ");
-      std::string display = input.length() >= 2 && input.front() == '"' && input.back() == '"' || input.front() == '\'' && input.back() == '\'' ? input.substr(1, input.length() - 2) : input.substr(n + std::string("echo ").length());
+      std::string display = input.length() >= 2 && args[1].front() == '"' && args[1].back() == '"' || args[1].front() == '\'' && args[1].back() == '\'' ? args[1].substr(1, args[1].length() - 2) : args[1];
 
       // std::string display = args[1];
       // std::cout << args[1] <<std::endl;
